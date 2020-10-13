212a950bb5c70f31f252c4c1a01a4c9b4e309194
Josh Pressnell <josh.pressnell@penthera.com>
2020-10-13 11:11:54 -0400

    asdf

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "VirtuosoClientDownloadEngine",
-            url: "https://github.com/josh-penthera/SPM_Test/releases/download/v1.0.1/VirtuosoClientDownloadEngine.xcframework.zip",
-            checksum: "f97d3a0403fe544f576cb097d2ccae5c5247da8bb1f959131f3cd91b6ddcec6e"
+            url: "https://github.com/josh-penthera/SPM_Test/releases/download/v1.0.3/VirtuosoClientDownloadEngine-spm.xcframework.zip",
+            checksum: "41b547d7fe5a7606bc4d9721f38a047dc46c9a212f922078980eba506b924a56"
         )
     ]
 )
